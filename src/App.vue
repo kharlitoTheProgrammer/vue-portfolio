c3bfbe010dadd865a8ceb9ca69bffcb14f164cb7
kharl <kharlchrisanaquino@gmail.com>
2025-04-20 21:50:07 +0800

    First commit, start of portfolio

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { HomeIcon, RectangleStackIcon, UserIcon } from '@heroicons/vue/24/solid'
+import { ref } from 'vue'
+
+const navItems = ref([
+  { name: 'home', icon: HomeIcon },
+  { name: 'projects', icon: RectangleStackIcon },
+  { name: 'profile', icon: UserIcon },
+  { name: 'home', icon: HomeIcon },
+  { name: 'home', icon: HomeIcon },
+])
+</script>
+
+<template>
+  <main class="bg-black min-h-screen pt-5">
+    <header>
+      <nav class="bg-[#222222] text-white h-[3rem] w-60 mx-auto rounded-xl">
+        <ul class="flex justify-evenly items-center h-full">
+          <li
+            class="flex items-center justify-center hover:bg-white hover:text-[#222222] rounded-xl p-3 transition duration-200 cursor-pointer"
+            v-for="(item, index) in navItems"
+            :key="index"
+          >
+            <component :is="item.icon" class="w-6 h-6" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </main>
+</template>
