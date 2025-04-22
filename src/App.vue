ff29b0d2f3c83b060f5fd260da6663e06895eed2
kharl <kharlchrisanaquino@gmail.com>
2025-04-22 14:02:30 +0800

    Update: Home content

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import { HomeIcon, RectangleStackIcon, UserIcon } from '@heroicons/vue/24/solid'
+import { Linkedin, Github } from 'lucide-vue-next'
 import { ref } from 'vue'
 
 const navItems = ref([
-  { name: 'home', icon: HomeIcon, path: '/' },
-  { name: 'projects', icon: RectangleStackIcon, path: '/projects' },
-  { name: 'profile', icon: UserIcon, path: '/profile' },
-  { name: 'home', icon: HomeIcon, path: '/' },
-  { name: 'home', icon: HomeIcon, path: '/' },
+  { name: 'home', icon: HomeIcon, path: '/', isExternal: false },
+  { name: 'projects', icon: RectangleStackIcon, path: '/projects', isExternal: false },
+  { name: 'profile', icon: UserIcon, path: '/profile', isExternal: false },
+  {
+    name: 'linkedin',
+    icon: Linkedin,
+    path: 'https://www.linkedin.com/in/kharl-chris-an-aquino-b6617625b',
+    isExternal: true,
+  },
+  {
+    name: 'home',
+    icon: Github,
+    path: 'https://www.github.com/kharlitoTheProgrammer',
+    isExternal: true,
+  },
 ])
 </script>
 
@@ -16,16 +27,24 @@ const navItems = ref([
     <header>
       <nav class="bg-[#222222] text-white h-[3rem] w-60 mx-auto rounded-xl">
         <ul class="flex justify-evenly items-center h-full">
-          <RouterLink
-            class="flex items-center justify-center hover:bg-white hover:text-[#222222] rounded-xl p-3 transition duration-200 cursor-pointer"
-            v-for="(item, index) in navItems"
-            :key="index"
-            :to="item.path"
-          >
-            <RouterLink :to="item.path">
+          <li v-for="(item, index) in navItems" :key="index">
+            <RouterLink
+              class="flex items-center justify-center hover:bg-white hover:text-[#222222] rounded-xl p-3 transition duration-200 cursor-pointer"
+              :to="item.path"
+              v-if="!item.isExternal"
+            >
               <component :is="item.icon" class="w-6 h-6" />
             </RouterLink>
-          </RouterLink>
+            <a
+              :href="item.path"
+              v-else
+              target="_blank"
+              rel="noopener noreferrer"
+              class="flex items-center justify-center hover:bg-white hover:text-[#222222] rounded-xl p-3 transition duration-200 cursor-pointer"
+            >
+              <component :is="item.icon" class="w-6 h-6" />
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="text-white">Home</div>
+  <div
+    class="text-[#f0f0f0] font-['Satoshi'] flex flex-col justify-center items-center gap-5 my-10 mx-10"
+  >
+    <h1 class="text-5xl text-gray-400">Hello, I am</h1>
+    <span class="text-8xl">Kharl</span>
+    <p class="text-sm text-justify">
+      Crafting clean, responsive interfaces with a focus on usability and simplicity. Experienced in
+      building modern web applications using Vue.js, Tailwind CSS, and API integration to create
+      seamless user experiences.
+    </p>
+  </div>
 </template>
