bb6b6f58ab95589e9c0ee50f991cd858d0c71f25
kharl <kharlchrisanaquino@gmail.com>
2025-04-23 23:51:07 +0800

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-portfolio',
+
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
